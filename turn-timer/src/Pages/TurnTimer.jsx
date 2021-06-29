a9e450a484158342373c90cf233679f1fe3fbf38
hescheel <hescheel@scheels.com>
2021-06-29 13:43:23 -0500

    TurnTimer Update

--- turn-timer/src/Pages/TurnTimer.jsx
+++ turn-timer/src/Pages/TurnTimer.jsx
@@ -4,7 +4,8 @@ import "../Pages_Styling/TurnTimer.css"
 import PlayerTimer from './PlayerTimer';
 class TimeoutComponent extends Component {
   state = {
-    countDown: 1,
+    countDown: null,
+    extraPool: null,
     running: false,
     currentIndex: 0,
     maxIndex: this.props.players.length-1 || 0,
@@ -16,10 +17,28 @@ class TimeoutComponent extends Component {
       this.setState({currentIndex:0})
     }
   }
+  startTimer=()=>{
+    this.setState({
+      running:true
+    })
+  }
+  setCountDown=(event)=>{
+    this.setState({
+      countDown:event.target.value
+    })
+  }
+  setExtraPool=(event)=>{
+    this.setState({
+      extraPool:event.target.value
+    })
+  }
   render() {
     return (
       <div className="timer">
-        {this.props.players.map(x => <PlayerTimer nextPlayer={this.nextPlayer} myTurn={this.state.currentIndex === this.props.players.indexOf(x)} name={x} time={this.state.countDown} extraPool={1}></PlayerTimer>)}
+        <input className="input" placeholder="Time Per Turn" value={this.state.countDown} onChange={(event)=>this.setCountDown(event)}/>
+        <input className="input" placeholder="Extra Time Pool" value={this.state.extraPool} onChange={(event)=>this.setExtraPool(event)}/>
+        <button onClick={()=>this.startTimer()}>Start Combat</button>
+        {this.state.running && this.props.players.map(x => <PlayerTimer nextPlayer={this.nextPlayer} myTurn={this.state.currentIndex === this.props.players.indexOf(x) } name={x} time={this.state.countDown} extraPool={this.state.extraPool}></PlayerTimer>)}
       </div>
     );
   }
