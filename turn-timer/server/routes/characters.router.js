b8d4b08ff6de2d3f66c47574c023cb76c3a54b4f
JaegerScheel <hescheel@scheels.com>
2021-06-30 19:49:05 -0500

    character router changes

--- turn-timer/server/routes/characters.router.js
+++ turn-timer/server/routes/characters.router.js
@@ -4,7 +4,7 @@ const pool = require('../modules/pool.js');
 
 // GET Route
 router.get('/all', (req, res) => {
-    const queryText = 'SELECT ch.*, cl.classname FROM character ch INNER JOIN class cl on ch.class = cl.id';
+    const queryText = 'SELECT ch.*, cl.classname FROM character ch INNER JOIN class cl on ch.class_id = cl.id';
     pool
         .query(queryText)
         .then((result) => {
@@ -17,4 +17,19 @@ router.get('/all', (req, res) => {
 
 });
 
+// GET Route
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const queryText = 'SELECT ch.*, cl.classname FROM character ch INNER JOIN class cl on ch.class_id = cl.id INNER JOIN campaign cm on ch.campaign_id = cm.id where ch.id = $1';
+    pool
+        .query(queryText, [id])
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error while making query: ${queryText}\nERRCODE: ${error}`);
+            res.sendStatus(500);
+        })
+
+});
 module.exports = router;
